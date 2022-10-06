3287d5506420a7b15478e402de83c9dd8017b864
k1rill-dev <63358980+k1rill-dev@users.noreply.github.com>
2022-10-06 21:49:19 +0300

    upd

--- DubnaWebApp/webapp/models.py
+++ DubnaWebApp/webapp/models.py
@@ -1,10 +1,10 @@
 from decimal import Decimal
-
 from dj_shop_cart.cart import CartItem
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -14,11 +14,13 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.tg_username}"
 
-#signals to User model
+
+# signals to User model
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
@@ -35,6 +37,7 @@ class Order(models.Model):
 
     def __str__(self):
         return f"{self.order_list[:10]}; {self.owner.tg_username}"
+
 
 class Restaurant(models.Model):
     title = models.CharField(max_length=255, blank=True)
