bd01b7328064f9c320fe5431c496a2ce29091126
k1rill-dev <63358980+k1rill-dev@users.noreply.github.com>
2022-10-09 23:40:35 +0300

    фиксим
    починил и добавил поиск + мелкие баги тоже исправлены

--- DubnaWebApp/webapp/forms.py
+++ DubnaWebApp/webapp/forms.py
@@ -6,7 +6,7 @@ from .models import Profile
 class MakeOrder(forms.ModelForm):
     address = forms.CharField(widget=forms.TextInput(attrs={'class': 'field','placeholder':'Адрес доставки'}), label='')
     full_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'field','placeholder':'Имя и Фамилия получателя'}), label='')
-    phone_number = forms.CharField(widget=forms.TextInput(attrs={'class': 'field', 'placeholder':'Номер телефона'}), label='')
+    phone_number = forms.CharField(widget=forms.NumberInput(attrs={'class': 'field', 'placeholder':'Номер телефона'}), label='')
     comments = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': "Любые пожелания, детали и т.д."}),
         label='')
     class Meta:
--- DubnaWebApp/webapp/models.py
+++ DubnaWebApp/webapp/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 class Profile(models.Model):
     # user = models.OneToOneField(User, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=255, blank=True)
-    tg_username = models.CharField(max_length=255, blank=True)
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+    tg_username = models.CharField(max_length=255, blank=True, null=True)
     tg_id = models.IntegerField(blank=True, null=True, default=0)
-    address = models.CharField(blank=True, max_length=255)
+    address = models.CharField(blank=True, max_length=255, null=True)
     bonus = models.IntegerField(blank=True, null=True, default=0)
-    phone_number = models.BigIntegerField(blank=True)
-    is_stuff = models.BooleanField(default=False, blank=True)
+    phone_number = models.BigIntegerField(blank=True, null=True)
+    is_stuff = models.BooleanField(default=False, blank=True, null=True)
 
     def __str__(self):
         return f"{self.tg_username}"
