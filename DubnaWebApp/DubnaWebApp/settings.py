08e5fc0cb0282dd571f9f952d1e607c6a8d03072
kirill <userkirill609@gmail.com>
2022-10-15 02:23:36 +0300

    upload dockerfile

--- DubnaWebApp/DubnaWebApp/settings.py
+++ DubnaWebApp/DubnaWebApp/settings.py
@@ -79,26 +79,34 @@ WSGI_APPLICATION = 'DubnaWebApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'dubna_hackaton',
-#         'USER': 'postgres',
-#         'PASSWORD': 'KOKI___KLAVY123',
-#         'HOST': 'localhost',
-#         'PORT': 5432,
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd1bku3sv3h5gug',
-        'USER': 'mchdxzgiplfwal',
-        'PASSWORD': '62a4b84119b5d0420e513481504d6dde8fb1b7c1b7371f289453e1aa0696acf2',
-        'HOST': 'ec2-52-4-87-74.compute-1.amazonaws.com',
+        'NAME': 'hello_django_dev',
+        'USER': 'hello_django',
+        'PASSWORD': 'hello_django',
+        'HOST': 'db',
         'PORT': 5432,
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'd1bku3sv3h5gug',
+#         'USER': 'mchdxzgiplfwal',
+#         'PASSWORD': '62a4b84119b5d0420e513481504d6dde8fb1b7c1b7371f289453e1aa0696acf2',
+#         'HOST': 'ec2-52-4-87-74.compute-1.amazonaws.com',
+#         'PORT': 5432,
+#     }
+# }
 # если что, фейк инфа о базе данных))
 
 # Password validation
